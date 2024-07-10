7f3d19c2047812d22c5b569af4746c1fb159d735
Prashant <97339424+prashant-2204@users.noreply.github.com>
2024-07-10 23:34:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post("/project", async (req, res) => {
   await ecsClient.send(command);
   return res.json({
     status: "queued",
-    data: { projectSlug, url: `https://vercel-clone-pk.s3.eu-north-1.amazonaws.com/__outputs/${projectSlug}` }, //https://vercel-s3-reverse-proxy.onrender.com
+    data: { projectSlug, url: `https://vercel-clone-pk.s3.eu-north-1.amazonaws.com/__outputs/${projectSlug}/index.html` }, //https://vercel-s3-reverse-proxy.onrender.com
   });
 });
 
